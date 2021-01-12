9317be44bc68f5592d1b0544d283cac9b179ba03
김준원 <junis3@snu.ac.kr>
2021-01-13 02:19:28 +0900

    Added additional comments on midnight daily PS review

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { getProblemInfo } from "./getProblemInfo";
 
 const app = express();
 
+const csGod = 'UV78YL6TW';
 const cstodoChannel = 'C01H4RY69CL';
 const cstodoTestChannel = 'C01JER4T7AN';
 
@@ -126,10 +127,27 @@ schedule.scheduleJob('0 0 0 * * *', async () => {
 
   const today = await Promise.all(currentHistory.filter((value) => !history.find((item) => item === value)).map(async (id) => await getProblemInfo(Number.parseInt(id))));
 
+  const diamonds = today.filter((item) => item.level.includes('dia'));
+  const rubys = today.filter((item) => item.level.includes('ruby'));
+
   webClient.chat.postMessage({
     text: '오늘 :god: :시신: :god:님이 푼 문제들: ' + today.map((problem) => `<http://icpc.me/${problem.id}|:${problem.level}:${problem.title}>`).join(', '),
     channel: cstodoChannel,
-  })
+  });
+
+  if (diamonds.length === 0 && rubys.length === 0) {
+    webClient.chat.postMessage({
+      text: '히잉.... cs님이 오늘 다이아를 안 푸셔서 슬랙봇 마음이 너무 아파요... 내일은 꼭 다이아 풀어주시는 거죠? :blobcry: :blobhug: :시신:',
+      channel: cstodoChannel,
+    });
+  }
+
+  rubys.forEach((problem) => {
+    webClient.chat.postMessage({
+      text: `:tada: cs신님께 새로 학살당한 루비! <http://icpc.me/${problem.id}|:${problem.level}:${problem.title}> 입니다! :시신: :tada:`,
+      channel: cstodoChannel,
+    });
+  });
 });
 
 app.use('/cstodo', slackEvents.requestListener());

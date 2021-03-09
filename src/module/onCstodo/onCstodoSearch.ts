39f4adf74495b0cc12b5234a89ddb4fb58e54653
김준원 <junis3@snu.ac.kr>
2021-03-09 17:24:12 +0900

    Changed search result message

--- src/module/onCstodo/onCstodoSearch.ts
+++ src/module/onCstodo/onCstodoSearch.ts
@@ -7,36 +7,30 @@ const onCstodoSearch = async (event: any) => {
     const tokens = text.split(' ').map((token) => token.trim());
     const query = tokens.slice(2).join(' ').trim();
 
-    const cstodo = await getCstodos();
+    const cstodos = await getCstodos();
     
-    
-    const result = await new Promise<CstodoType[] | null>(async (resolve, reject) => {
-      setTimeout(() => {
-        resolve(null);
-      }, 3000);
+    const result = await new Promise<CstodoType[] | null>((resolve, reject) => {
+        setTimeout(() => resolve(null), 3000);
 
-      getCstodos().then((cstodo) => {
-        const result = cstodo.filter((todo) => {
+        const result = cstodos.filter((todo) => {
           return todo.content.search(new RegExp(String.raw`${query}`)) !== -1;
         });
         resolve(result);
-      });
     });
 
     let message: string;
     let icon_emoji = emoji('default');
     let username = 'cstodo';
 
-    console.log(result);
     if (result === null) {
       message = '무슨 검색어를 넣었길래 이렇게 오래 걸려요?;;;';
       icon_emoji = emoji('ddokddul');
       username = '똑떨한 cstodo'
     } else if (result.length === 0) {
-      message = `${emoji('cs')}님의 할 일에 찾으시는 ${query}가 없습니다..ㅠㅠ`;
+      message = `${emoji('cs')}님의 할 일에 찾으시는 '${query}'가 없습니다..ㅠㅠ`;
     } else {
 
-      message = `${emoji('cs')}님의 할 일에서 ${query}를 검색한 결과입니다:`;
+      message = `${emoji('cs')}님의 할 일에서 '${query}'를 검색한 결과입니다:`;
       message += result.map((value) => `\n- ${value.content}`);  
     }
 

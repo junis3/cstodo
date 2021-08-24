fc45740c90fcd266b73653a878d3301bc9ffa8af
김준원 <junis3@snu.ac.kr>
2021-08-24 23:18:43 +0900

    Changed help text

--- src/module/todo/onTodoHelp.ts
+++ src/module/todo/onTodoHelp.ts
@@ -8,12 +8,12 @@ const helpText = (user: UserType) => {
     return `${name}님의 비서 - ${command} 봇 사용설명서
 \`${command}\`: ${name}님의 할 일 목록 일부를 볼 수 있습니다.
 \`${command} all\`: ${name}님의 할 일 목록 전체를 볼 수 있습니다.
-\`${command} blob\` 또는 \`${command} weeb\`: ${command} 봇의 프로필을 바꿀 수 있습니다.
-\`${command} mute\` 또는 \`${command} unmute\`: ${command} 봇이 시끄러울 때에, 응답을 조용히 전달하게 할 수 있습니다.
-\`${command} size\`: ${name}님의 할 일님의 개수를 볼 수 있습니다.
+\`${command} size\`: ${name}님의 할 일의 개수를 볼 수 있습니다.
 \`${command} search\`: ${name}님의 할 일에 들어있는 항목을 정규표현식으로 검색할 수 있습니다.
 \`${command} add [내용]\`: ${name}님의 할 일 목록에 새로운 항목을 넣을 수 있습니다.
-\`${command} remove [내용]\`: ${name}님의 할 일 목록에 항목을 뺄 수 있습니다.`
+\`${command} remove [내용]\`: ${name}님의 할 일 목록에 항목을 뺄 수 있습니다.
+${command === 'cstodo' ? `\`${command} mute\` 또는 \`${command} unmute\`: ${command} 봇이 시끄러울 때에, 응답을 조용히 전달하게 할 수 있습니다.` : ''}
+${command === 'cstodo' ? `\`${command} blob\` 또는 \`${command} weeb\`: ${command} 봇의 프로필을 바꿀 수 있습니다.` : ''}`
 }
   
 const onCstodoHelp = async (event: any, user: UserType) => {

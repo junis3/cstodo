e777fa85f1e0c694e459f989a19f6a5252b3e65c
김준원 <junis3@snu.ac.kr>
2021-08-26 01:59:41 +0900

    Blocked thread echo with messages over 5 minutes elapsed

--- src/module/echo/index.ts
+++ src/module/echo/index.ts
@@ -3,6 +3,7 @@ import isAttack from '../isAttack';
 
 const onEcho = async (event: any) => {
     if (await isAttack(event)) return;
+    if (event.thread_ts && Number.parseFloat(event.thread_ts) < (new Date().getTime() / 1000) - 5 * 60) return;
     
     const text : string = event.text;
     const tokens = text.split(' ').map((token) => token.trim());

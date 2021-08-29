dd48a1bb1ecc1d263d2b53979a5cc67e4323f06b
김준원 <junis3@snu.ac.kr>
2021-08-30 03:33:01 +0900

    Changed help text

--- src/module/todo/onTodoHelp.ts
+++ src/module/todo/onTodoHelp.ts
@@ -13,8 +13,8 @@ const helpText = (user: UserType) => {
 \`${command} search\`: ${name}님의 할 일에 들어있는 항목을 정규표현식으로 검색할 수 있습니다.
 \`${command} add [내용]\`: ${name}님의 할 일 목록에 새로운 항목을 넣을 수 있습니다.
 \`${command} remove [내용]\`: ${name}님의 할 일 목록에 항목을 뺄 수 있습니다.
-${command === 'cstodo' ? `\`${command} mute\` 또는 \`${command} unmute\`: ${command} 봇이 시끄러울 때에, 응답을 조용히 전달하게 할 수 있습니다.` : ''}
-${command === 'cstodo' ? `\`${command} blob\` 또는 \`${command} weeb\`: ${command} 봇의 프로필을 바꿀 수 있습니다.` : ''}`
+\`${command} mute\` 또는 \`${command} unmute\`: ${command} 봇이 시끄러울 때에, 응답을 조용히 전달하게 할 수 있습니다.
+\`${command} blob\` 또는 \`${command} weeb\`: ${command} 봇의 프로필을 바꿀 수 있습니다.`
 }
   
 const onTodoHelp = async (query: QueryType, event: any, user: UserType) => {

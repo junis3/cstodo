b63023350c94a2484e3fed7b4b2862963c7140b8
김준원 <junis3@snu.ac.kr>
2021-09-29 17:44:48 +0900

    Changed behaviour of todo set

--- src/module/todo/onTodoSet.ts
+++ src/module/todo/onTodoSet.ts
@@ -1,21 +1,24 @@
 import { setHome, setOwner, UserType } from '../../database/user';
 import { emoji } from '../../etc/theme';
 import { getArg, QueryType } from '../../etc/parseQuery';
-import { replyDdokddul, replySuccess } from '../../etc/postMessage';
+import { addEmoji, replyDdokddul, replySuccess } from '../../etc/postMessage';
 import { cstodoTestChannel } from '../../config';
 
 const onTodoSet = async ({ command, args }: QueryType, event: any, user: UserType) => {
 
-    if (event.user !== user.owner && event.user !== 'UV6HYQD3J')
+    if (event.user !== user.owner && event.user !== 'UV6HYQD3J') {
+        addEmoji(event, 'sad');
         return;
+    }
 
     const useDue = getArg(['-use-due', '--use-due', '-useDue', '--useDue'], args);
     const usePriority = getArg(['-use-priority', '--use-priority', '-usePriority', '--usePriority'], args);
     const useBar = getArg(['-use-bar', '--use-bar', '-useBar', '--useBar'], args);
-    const owner = getArg(['--owner'], args);
-    const home = getArg(['--home'], args);
+    const rawOwner = getArg(['--owner'], args);
+    const rawHome = getArg(['--home'], args);
 
-    if (typeof owner === 'string') {
+    if (typeof rawOwner === 'string' && (rawOwner.startsWith('<@') && rawOwner.endsWith('>'))) {
+        const owner = rawOwner.slice(2, -1);
         await setOwner(user.command, owner);
         if (user.owner) {
             await replyDdokddul(event, user, `저의 주인이 이미 <@${user.owner}>님으로 설정되어 있습니다!! 꼭 바꾸어야 한다면 <#${cstodoTestChannel}>에 말씀해주세요..`)
@@ -24,7 +27,8 @@ const onTodoSet = async ({ command, args }: QueryType, event: any, user: UserTyp
         }
     }
 
-    if (typeof home === 'string') {
+    if (typeof rawHome === 'string' && (rawHome.startsWith('<#') && rawHome.endsWith('>'))) {
+        const home = rawHome.slice(2, -1);
         await setHome(user.command, home);
         await replySuccess(event, user, `${user.name}님의 비서의 위치가 <#${home}>으로 설정되었습니다! 추후 봇 관련 큰 변화가 있을 때 이 채널로 전달될 예정입니다..`);
     }

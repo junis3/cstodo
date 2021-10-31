96e66796ae3a3cbf55e77df3df2745f3d485d413
김준원 <junis3@snu.ac.kr>
2021-11-01 01:15:04 +0900

    Modified cstodo help message

--- src/module/todo/onTodoHelp.ts
+++ src/module/todo/onTodoHelp.ts
@@ -9,12 +9,10 @@ const helpText = (user: UserType) => {
     const name = user.name;
     const command = user.command;
     return `${name}님의 비서 - ${command} 봇 사용설명서
-\`${command}\`: ${name}님의 할 일 목록 일부를 볼 수 있습니다.
-\`${command} all\`: ${name}님의 할 일 목록 전체를 볼 수 있습니다.
-\`${command} size\`: ${name}님의 할 일의 개수를 볼 수 있습니다.
-\`${command} search\`: ${name}님의 할 일에 들어있는 항목을 정규표현식으로 검색할 수 있습니다.
-\`${command} add [내용]\`: ${name}님의 할 일 목록에 새로운 항목을 넣을 수 있습니다.
-\`${command} remove [내용]\`: ${name}님의 할 일 목록에 항목을 뺄 수 있습니다.
+\`${command}\`: ${name}님의 할 일 목록을 볼 수 있습니다.
+\`${command} add [내용] -d [마감일]\`: ${name}님의 할 일 목록에 새로운 항목을 넣을 수 있습니다.
+\`${command} remove [번호]\`: ${name}님의 할 일 목록에 항목을 뺄 수 있습니다.
+\`${command} edit [번호] -c [수정할 내용] -d [수정할 마감일]\`: ${name}님의 할 일 목록에 있는 항목을 수정할 수 있습니다.
 \`${command} mute\` 또는 \`${command} unmute\`: ${command} 봇이 시끄러울 때에, 응답을 조용히 전달하게 할 수 있습니다.
 \`${command} blob\` 또는 \`${command} weeb\`: ${command} 봇의 프로필을 바꿀 수 있습니다.`
 }

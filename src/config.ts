4efc6ba2b325f49e0a9fa47e6ca6c95ea01e2122
namhyunuk <jwvg0425@naver.com>
2021-12-06 13:09:30 +0900

    add cstodo test channel to env config

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ export const port = Number.parseInt(process.env.PORT || '3000');
 export const isTesting = !process.env.IS_PRODUCTION;
 
 export const csGod = 'UV78YL6TW';
-export const cstodoTestChannel = 'C01JER4T7AN';
+export const cstodoTestChannel = process.env.CSTODO_TEST_CHANNEL || 'C01JER4T7AN';
 export const cstodoChannel = isTesting ? cstodoTestChannel : 'C01H4RY69CL';
 
 // export const green55 = '';

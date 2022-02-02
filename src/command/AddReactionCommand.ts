8ba5e5f2b38c0ce70db75a7bdc31b813f36c0ad9
김준원 <junis3@snu.ac.kr>
2022-02-03 01:15:32 +0900

    Fixed 'already_reacted' error

--- src/command/AddReactionCommand.ts
+++ src/command/AddReactionCommand.ts
@@ -3,7 +3,7 @@ import { CommandInterface, webClient } from '.';
 
 const isAcceptableError = (e: unknown) => typeof e === 'object' && e !== null
   && 'message' in e
-  && (e as any).message !== 'An API error occurred: already_reacted';
+  && (e as any).message === 'An API error occurred: already_reacted';
 
 // eslint-disable-next-line import/prefer-default-export
 export class AddReactionCommand implements CommandInterface {
@@ -17,8 +17,6 @@ export class AddReactionCommand implements CommandInterface {
     try {
       await webClient.reactions.add(this.props);
     } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error(e);
       if (!isAcceptableError(e)) throw e;
     }
     return null;

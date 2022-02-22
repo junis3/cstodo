0b971f576ffa5069175bf91d579d249c5b3f25f7
TAMREF <sluggeryck@naver.com>
2022-02-22 16:59:47 +0900

    enabled hw refresh for owners

--- src/module/todo/onTodoHW.ts
+++ src/module/todo/onTodoHW.ts
@@ -7,13 +7,7 @@ import { validateThenChooseProblem } from '../onDailyGreenGold';
 
 const onTodoHW: TodoRouter = async ({ query: { command, args }, event, user }) => {
   const forceRefreshArg = getArg(['--refresh', '-r'], args);
-  if(typeof forceRefreshArg == 'string') {
-    if(event.user !== 'UV6HYQD3J' && event.user !== 'UV8DYMMV5' && event.user != 'U02QVE5EDE0') {
-      return new ReplyFailureCommand(event, user, `숙제 갱신은 관리자만 할 수 있어요...`)
-    }
-    await validateThenChooseProblem(user.command);
-    return new ReplySuccessCommand(event, user, `숙제 갱신이 완료되었어요!`);
-  }
+
   const numProblems = user.numProbsPerCycle || 1;
   const problems = await getLatestGreenGolds(user.command, numProblems);
   if (problems === null
@@ -21,6 +15,16 @@ const onTodoHW: TodoRouter = async ({ query: { command, args }, event, user }) =
     || problems.length < numProblems) {
     return new ReplyFailureCommand(event, user, `${user.name}님은 숙제를 받은 적이 없어요... 관리자에게 다음 정보를 주시면 생성해드릴 거예요...\n- 숙제를 받고 싶은 주기와 시점\n- 숙제를 뽑는 solved.ac 쿼리\n- 한번에 받을 숙제 개수\n관리자가 귀찮아하면 지연될 수 있어요...`);
   }
+
+  if(typeof forceRefreshArg == 'string') {
+    if(event.user !== 'UV6HYQD3J' && event.user !== 'UV8DYMMV5' && event.user != 'U02QVE5EDE0'
+    && event.user != user.owner) {
+      return new ReplyFailureCommand(event, user, `숙제 갱신은 관리자와 주인만 할 수 있어요...`)
+    }
+    await validateThenChooseProblem(user.command);
+    return new ReplySuccessCommand(event, user, `숙제 갱신이 완료되었어요!`);
+  }
+
   const hrefs = await Promise.all(
     problems.map(
       (problem: GreenGoldType) => greenGoldToHrefNoLevel(problem),

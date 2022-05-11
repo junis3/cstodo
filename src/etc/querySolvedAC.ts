af5d94c9a619d26fb19f7bf45b0536a67a33c6fd
TAMREF <sluggeryck@naver.com>
2022-05-11 14:05:03 +0900

    replace '&' to url-encoded char in querySolvedAC

--- src/etc/querySolvedAC.ts
+++ src/etc/querySolvedAC.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const querySolvedAC = async (query = '') => {
     try {
-      return await axios.get(`https://solved.ac/api/v3/search/problem?query=${query}&sort=random`, {
+      const urlSafeQuery : string = query.split('&').join('%26');
+      console.log(urlSafeQuery)
+      return await axios.get(`https://solved.ac/api/v3/search/problem?query=${urlSafeQuery}&sort=random`, {
         headers: {
           'Content-Type': 'application/json',
         },
@@ -14,3 +16,9 @@ const querySolvedAC = async (query = '') => {
 };
 
 export default querySolvedAC;
+
+const f = async () => {
+  const x = await querySolvedAC(`(from:joisc|from:ktsc|tag:flow|(from:ioi&id:19929..&!(id:20067..20076)&!(id:20094..20103)&!(id:22307..22315))) -solved_by:koosaga`);
+  console.log(x.data.count);
+};
+f();

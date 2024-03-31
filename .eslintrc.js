587d00caef166d5e0b7a86983d79f688f0e7e48e
김준원 <junis3@snu.ac.kr>
2024-04-01 02:17:29 +0900

    대나무숲 기능 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,32 @@
-module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 13,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'require-jsdoc': 0,
-    'linebreak-style': 0,
-    'no-shadow': 0,
-    '@typescript-eslint/no-unused-vars': 2,
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        mjs: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {},
+export const env = {
+  es2021: true,
+  node: true,
+};
+export const parser = '@typescript-eslint/parser';
+export const parserOptions = {
+  ecmaVersion: 13,
+  sourceType: 'module',
+};
+export const plugins = ['@typescript-eslint'];
+export const rules = {
+  'require-jsdoc': 0,
+  'linebreak-style': 0,
+  'no-shadow': 0,
+  '@typescript-eslint/no-unused-vars': 2,
+  'import/extensions': [
+    'error',
+    'ignorePackages',
+    {
+      js: 'never',
+      mjs: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
     },
+  ],
+};
+export const settings = {
+  'import/resolver': {
+    typescript: {},
   },
 };
